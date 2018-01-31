97f2f18d3dab05e800d47cdfc7e6000626d3698b
Devin Hart <32463419+devin-hart@users.noreply.github.com>
2018-01-30 23:48:01 -0500

    hangman added

--- js/index.js
+++ js/index.js
@@ -7,6 +7,9 @@ var trippinDesc = document.querySelector('.trippin-desc');
 var bloggotronToggle = document.querySelector('.bloggotron-toggle');
 var bloggotronDesc = document.querySelector('.bloggotron-desc');
 
+var f13Toggle = document.querySelector('.f13-toggle');
+var f13Desc = document.querySelector('.f13-desc');
+
 sscToggle.addEventListener("click", function(){
   if (sscDesc.style.display === "block") {
     sscDesc.style.display = "none";
@@ -28,5 +31,13 @@ bloggotronToggle.addEventListener("click", function(){
     bloggotronDesc.style.display = "none";
   } else {
     bloggotronDesc.style.display = "block";
+  }
+});
+
+f13Toggle.addEventListener("click", function(){
+  if (f13Desc.style.display === "block") {
+    f13Desc.style.display = "none";
+  } else {
+    f13Desc.style.display = "block";
   }
 });
