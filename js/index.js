4b22e3a7d8516cf1039631b0f70ff98e935ff044
Devin Hart <32463419+devin-hart@users.noreply.github.com>
2018-01-31 15:24:43 -0500

    carousel loop

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-const siema = new Siema();
+const siema = new Siema({
+  loop: true
+});
 
 document.querySelector('.prev').addEventListener('click', () => siema.prev());
 document.querySelector('.next').addEventListener('click', () => siema.next());
