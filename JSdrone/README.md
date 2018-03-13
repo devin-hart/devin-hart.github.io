9afd0f1b3d1ae9f1c3c76f1b767d76a12ceb176e
Devin Hart <32463419+devin-hart@users.noreply.github.com>
2018-03-13 15:06:27 -0400

    JSDrone updated

--- JSdrone/README.md
+++ JSdrone/README.md
@@ -1,1 +1,45 @@
-# JSDrone
+# Call to the Void
+## JavaScript Drone Synthesizer
+
+Call to the Void is a 4 oscillator drone synthesizer using the Web Audio
+API through the Tone.js framework.
+
+### What's a drone?
+According to Merriam Webster a drone is a deep sustained or monotonous sound. By this definition a drone may sound dull but in practice drones are very important to creating atmosphere in music and movie soundtracks. Drones in combination with other drones can create eerie to pleasant atmospheric sounds.
+
+### Why a drone synth?
+Drone synths are a simple concept that can quickly create some interesting results by just turning a few knobs. There are also 100s of different online keyboard synthesizers on the internet and if you're not familiar with playing the piano these synthesizers aren't much fun beyond pressing each key and hearing each sound for a minute or two. So with Call to the Void you may get another 2 or 3 minutes of enjoyment out of this drone synth than you would another keyboard synthesizer! :D
+
+## Display
+### Oscillators Knobs
+
+The left side knobs control a different oscillators frequency.
+Frequency range goes from 16-370 hz or ~C0 - ~F#3 (note/octave.)
+This range was chosen based on the Grendel Drone Commander 1 frequency range.
+
+### Volume Knobs
+The right side knobs control a different oscillators volume.
+Volume ranges goes from -64 dB (inaudible)  to 0 dB (loudest) based on sound
+mixer level ranges.
+
+### PWM Knob
+The Pulse oscillator has a PWM (Pulse Width Modulation) knob which controls the
+duty cycle of the Pulse oscillator from 0 - 100.
+In depth explanation can be found [here.](https://www.keithmcmillen.com/blog/simple-synthesis-part-6-pulse-width-modulation/)
+
+### Lights
+The center lights will trigger red for "on" when the volume value is > -64dB for it's corresponding oscillator and black for "off" when the volume value is = -64 db.
+
+### Value boxes
+The value boxes display the corresponding oscillator's frequency or volume.  
+
+## Built with:
+- HTML
+- CSS
+- Vanilla JavaScript
+
+#### JavaScript framework/library:
+- [Tone.js](https://tonejs.github.io/)
+- [Input-Knobs.js](https://github.com/g200kg/input-knobs)
+
+Thank you to [Devon Gardner](https://github.com/Gradner) for helping me get my lights feature working properly.
--- JSdrone/javascript/index.js
+++ JSdrone/javascript/index.js
@@ -2,19 +2,27 @@
 // Sine
 const sineKnob = document.querySelector('#sine-knob');
 const sineVolKnob = document.querySelector('#sine-vol-knob');
+const sineOscVal = document.querySelector('#sine-osc-value');
+const sineVolVal = document.querySelector('#sine-vol-value');
 
 // Triangle
 const triangleKnob = document.querySelector('#triangle-knob');
 const triangleVolKnob = document.querySelector('#triangle-vol-knob');
+const triangleOscVal = document.querySelector('#triangle-osc-value');
+const triangleVolVal = document.querySelector('#triangle-vol-value');
 
 // Sawtooth
 const sawtoothKnob = document.querySelector('#sawtooth-knob');
 const sawtoothVolKnob = document.querySelector('#sawtooth-vol-knob');
+const sawtoothOscVal = document.querySelector('#sawtooth-osc-value');
+const sawtoothVolVal = document.querySelector('#sawtooth-vol-value');
 
 // Pulse
 const pulseKnob = document.querySelector('#pulse-knob');
 const pulseVolKnob = document.querySelector('#pulse-vol-knob');
 const pwmKnob = document.querySelector('#pwm-knob');
+const pulseOscVal = document.querySelector('#pulse-osc-value');
+const pulseVolVal = document.querySelector('#pulse-vol-value');
 
 // Lights
 const sineLight = document.querySelector('#sine-light');
@@ -46,45 +54,53 @@ const pulseOsc = new Tone.PWMOscillator({ frequency : 0,
 // Frequency Knobs
 sineKnob.addEventListener('input', () => {
   sineOsc.frequency.value = sineKnob.value;
+  sineOscVal.textContent = sineKnob.value;
   console.log("Sine frequency: " + sineOsc.frequency.value);
 });
 
 triangleKnob.addEventListener('input', () => {
   triangleOsc.frequency.value = triangleKnob.value;
+  triangleOscVal.textContent = triangleKnob.value;
   console.log("Triangle frequency: " + triangleOsc.frequency.value);
 });
 
 sawtoothKnob.addEventListener('input', () => {
   sawtoothOsc.frequency.value = sawtoothKnob.value;
+  sawtoothOscVal.textContent = sawtoothKnob.value;
   console.log("Sawtooth frequency: " + sawtoothOsc.frequency.value);
 });
 
 pulseKnob.addEventListener('input', () => {
   pulseOsc.frequency.value = pulseKnob.value;
+  pulseOscVal.textContent = pulseKnob.value;
   console.log("Pulse frequency: " + pulseOsc.frequency.value);
 });
 
 // Volume Knobs
 sineVolKnob.addEventListener('input', () => {
   sineOsc.volume.value = sineVolKnob.value;
+  sineVolVal.textContent = sineVolKnob.value;
   console.log("Sine volume: " + Math.ceil(sineOsc.volume.value));
   lightOn(sineVolKnob.value, sineLight);
 });
 
 triangleVolKnob.addEventListener('input', () => {
   triangleOsc.volume.value = triangleVolKnob.value;
+  triangleVolVal.textContent = triangleVolKnob.value;
   console.log("Triangle volume: " + triangleOsc.volume.value);
   lightOn(triangleVolKnob.value, triangleLight);
 });
 
 sawtoothVolKnob.addEventListener('input', () => {
   sawtoothOsc.volume.value = sawtoothVolKnob.value;
+  sawtoothVolVal.textContent = sawtoothVolKnob.value;
   console.log("Sawtooth volume: " + sawtoothOsc.volume.value);
   lightOn(sawtoothVolKnob.value, sawtoothLight);
 });
 
 pulseVolKnob.addEventListener('input', () => {
   pulseOsc.volume.value = pulseVolKnob.value;
+  pulseVolVal.textContent = pulseVolKnob.value;
   console.log("Pulse volume: " + pulseOsc.volume.value);
   lightOn(pulseVolKnob.value, pulseLight);
 });
@@ -92,15 +108,12 @@ pulseVolKnob.addEventListener('input', () => {
 // PWM
 pwmKnob.addEventListener('input', () => {
   pulseOsc.modulationFrequency.value = pwmKnob.value;
-  console.log("PWM Duty Cycle: " + pulseOsc.modulationFrequency.value); });
+  console.log("PWM: " + pulseOsc.modulationFrequency.value); });
 
 function lightOn(knobVal, knobElement) {
-  console.log(knobVal);
   if (Math.ceil(knobVal) > -64) {
     knobElement.style.backgroundColor = "red";
-    return console.log('this should work');
   } else {
     knobElement.style.backgroundColor = "black";
-    console.log('not working')
   }
 };
